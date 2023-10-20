a5537af86e97c90fb62a17c029f3be966d386b16
IgnaDev <arcortoon@gmail.com>
2023-10-20 15:40:14 -0300

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://NachoBasilio.github.io',
-  base: '/landing-page-astro-fazt',
+  base: 'landing-page-astro-fazt',
 });
